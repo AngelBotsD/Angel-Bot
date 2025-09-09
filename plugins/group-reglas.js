51748ce3986bc5e38f74a975c7278d181f34aa39
AngelBotsD <angelperez22448@gmail.com>
2025-09-09 00:24:40 -0600

    Actualizar group-reglas.js

--- plugins/group-reglas.js
+++ plugins/group-reglas.js
@@ -1,14 +1,26 @@
-let handler = async (m, { conn }) => {
-    let chat = global.db.data.chats[m.chat]
-        if (chat.sRules) {
-            let reglas = chat.sRules;
-            m.reply(reglas);
-        } else {
-            m.reply('𝙀𝙡 𝙜𝙧𝙪𝙥𝙤 𝙣𝙤 𝙩𝙞𝙚𝙣𝙚 𝙧𝙚𝙜𝙡𝙖𝙨');
-        }
+let handler = async (m, { conn, text, isROwner, isOwner }) => {
+    let chat = global.db.data.chats[m.chat];
+
+    // Si se envía texto, se actualizan las reglas
+    if (text) {
+        chat.sRules = text;
+        conn.reply(m.chat, `𝙍𝙀𝙂𝙇𝘼𝙎 𝘼𝘾𝙏𝙐𝘼𝙇𝙄𝙕𝘼𝘿𝘼𝙎 ✅`, m);
+        return;
+    }
+
+    // Si no se envía texto, se muestran las reglas actuales
+    if (chat.sRules) {
+        conn.reply(m.chat, chat.sRules, m);
+    } else {
+        conn.reply(m.chat, '𝙀𝙡 𝙜𝙧𝙪𝙥𝙤 𝙣𝙤 𝙩𝙞𝙚𝙣𝙚 𝙧𝙚𝙜𝙡𝙖𝙨', m);
+    }
 }
-handler.help = ['reglas']
-handler.tags = ['group']
-handler.command = ['reglas']
-handler.group = true
-export default handler
+
+handler.help = ['reglas', 'setreglas + Texto'];
+handler.tags = ['group'];
+handler.command = ['reglas', 'setreglas', 'nuevasreglas'];
+handler.group = true;
+handler.botAdmin = true; // Para actualizar reglas si quieres que solo admins puedan
+handler.admin = true;
+
+export default handler;
