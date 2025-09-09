4d89ef4c2444f48ee898edfeaf390ad84a486899
AngelBotsD <angelperez22448@gmail.com>
2025-09-08 23:45:11 -0600

    Actualizar sticker-sticker.js

--- plugins/sticker-sticker.js
+++ plugins/sticker-sticker.js
@@ -61,21 +61,6 @@ const handler = async (msg, { conn }) => {
 
     await conn.sendMessage(chatId, { sticker: { url: sticker } }, { quoted: msg });
 
-  await conn.sendMessage(m.chat, {
-    image: { url: img },
-    caption: texto,
-    contextInfo: {
-      externalAdReply: {
-        title: "𝗕𝗔𝗞𝗜 - 𝗕𝗢𝗧",
-        body: "",
-        thumbnailUrl: 'https://files.catbox.moe/jfov52.jpg',
-        sourceUrl: '',
-        mediaType: 1,
-        renderLargerThumbnail: false
-      }
-    }
-  }, { quoted: m })
-
     await conn.sendMessage(chatId, { react: { text: '✅', key: msg.key } });
 
   } catch (err) {
