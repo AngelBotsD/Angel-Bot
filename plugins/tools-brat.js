68192e7a54e6d57a3f778247c58e39ff548abae6
AngelBotsD <angelperez22448@gmail.com>
2025-09-09 20:05:33 -0600

    Actualizar tools-brat.js

--- plugins/tools-brat.js
+++ plugins/tools-brat.js
@@ -1,48 +1,39 @@
-const handler = async (m, { conn }) => {
-  const body = m.text?.trim()
-  if (!body) return
-
-  if (!/^brat|.brat\s+/i.test(body)) return
-
-  const text = body.replace(/^(brat|.brat)\s+/i, "").trim()
-  if (!text) {
-    return m.reply(`☁️ 𝘼𝙂𝙍𝙀𝙂𝘼 𝙏𝙀𝙓𝙏𝙊 𝙋𝘼𝙍𝘼 𝙂𝙀𝙉𝙀𝙍𝘼𝙍 𝙀𝙇 𝙎𝙏𝙄𝘾𝙆𝙀𝙍\n\nEjemplo: brat angelito`)
+const handler = async (m, { conn, args }) => {
+  if (!args[0]) {
+    return m.reply(`𝐀𝐠𝐫𝐞𝐠𝐚 𝐓𝐞𝐱𝐭𝐨 𝐏𝐚𝐫𝐚 𝐆𝐞𝐧𝐞𝐫𝐚𝐫 𝐄𝐥 𝐒𝐭𝐢𝐜𝐤𝐞𝐫 ☁️`)
   }
 
   try {
-    // reacción ⌛
-    await conn.sendMessage(m.chat, { react: { text: "⌛", key: m.key } })
-
-    const url = `https://api.siputzx.my.id/api/m/brat?text=${encodeURIComponent(text)}`
-
-      await conn.sendMessage(m.chat, {
-    image: { url: img },
-    caption: texto,
-    contextInfo: {
-      externalAdReply: {
-        title: "𝗕𝗔𝗞𝗜 - 𝗕𝗢𝗧",
-        body: "",
-        thumbnailUrl: img,
-        sourceUrl: '',
-        mediaType: 1,
-        renderLargerThumbnail: false
+    // Reacción ⌛ al mensaje original
+    await conn.sendMessage(m.chat, {
+      react: {
+        text: '⌛',
+        key: m.key
       }
-    }
-  }, { quoted: m })
+    })
 
-    // reacción ✅
-    await conn.sendMessage(m.chat, { react: { text: "✅", key: m.key } })
+    const url = `https://api.siputzx.my.id/api/m/brat?text=${encodeURIComponent(args.join(' '))}`
+    await conn.sendMessage(m.chat, {
+      sticker: { url },
+      packname: '',
+      author: '',
+    }, { quoted: m })
+
+    // Reemplaza ⌛ por ✅
+    await conn.sendMessage(m.chat, {
+      react: {
+        text: '✅',
+        key: m.key
+      }
+    })
   } catch (e) {
     console.error(e)
-    await conn.sendMessage(m.chat, { react: { text: "❌", key: m.key } })
-    conn.reply(m.chat, '❌ *la api está caída*', m)
+    conn.reply(m.chat, '𝐇𝐮𝐛𝐨 𝐔𝐧 𝐄𝐫𝐫𝐨𝐫 𝐀𝐥 𝐆𝐞𝐧𝐞𝐫𝐚𝐫 𝐄𝐥 𝐒𝐭𝐢𝐜𝐤𝐞𝐫 ❌', m)
   }
 }
 
-// igual que play: brat <texto> o .brat <texto>
-handler.customPrefix = /^(brat|.brat)\s+/i
-handler.command = new RegExp
-handler.help = ["brat <texto>"]
-handler.tags = ["sticker"]
+handler.command = /^brat$/i
+handler.help = ['brat <texto>']
+handler.tags = ['sticker']
 
 export default handler
