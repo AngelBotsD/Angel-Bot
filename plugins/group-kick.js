d2d68cd9a90c8c784ab8554326e43b8eaac193f3
AngelBotsD <angelperez22448@gmail.com>
2025-09-10 11:01:12 -0600

    Actualizar group-kick.js

--- plugins/group-kick.js
+++ plugins/group-kick.js
@@ -3,11 +3,26 @@ const handler = async (m, { conn }) => {
     ? m.mentionedJid[0]
     : m.quoted?.sender;
 
-  if (!target) return m.reply('*🗡️ 𝙼𝚎𝚗𝚌𝚒𝚘𝚗𝚊 𝚘 𝚛𝚎𝚜𝚙𝚘𝚗𝚍𝚎 𝙰𝚕 𝚞𝚜𝚞𝚊𝚛𝚒𝚘 𝚚𝚞𝚎 𝙳𝚎𝚜𝚎𝚊𝚜 𝙴𝚕𝚒𝚖𝚒𝚗𝚊𝚛*', m, rcanal);
+  if (!target) {
+    const aviso = '*🗡️ 𝙼𝚎𝚗𝚌𝚒𝚘𝚗𝚊 𝚘 𝚛𝚎𝚜𝚙𝚘𝚗𝚍𝚎 𝙰𝚕 𝚞𝚜𝚞𝚊𝚛𝚒𝚘 𝚚𝚞𝚎 𝙳𝚎𝚜𝚎𝚊𝚜 𝙴𝚕𝚒𝚖𝚒𝚗𝚊𝚛*';
+    await conn.sendMessage(m.chat, {
+      text: aviso,
+      contextInfo: {
+        externalAdReply: rcanal   // aquí metes tu objeto
+      }
+    }, { quoted: m });
+    return;
+  }
 
   try {
     await conn.groupParticipantsUpdate(m.chat, [target], 'remove');
-    return m.reply('*☠️ 𝙸𝚗𝚞𝚝𝚒𝚕 𝙴𝚕𝚒𝚖𝚒𝚗𝚊𝚍𝚘*');
+    const eliminado = '*☠️ 𝙸𝚗𝚞𝚝𝚒𝚕 𝙴𝚕𝚒𝚖𝚒𝚗𝚊𝚍𝚘*';
+    await conn.sendMessage(m.chat, {
+      text: eliminado,
+      contextInfo: {
+        externalAdReply: rcanal   // igual aquí
+      }
+    }, { quoted: m });
   } catch {
     return global.dfail('botAdmin', m, conn);
   }
